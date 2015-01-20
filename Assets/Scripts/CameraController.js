04236f8d5d41d97472997e1bb0b2967ac0dafe3f
Pierre Reimertz <pierre@pierrereimertz.com>
2015-01-20 02:39:00 +0100

    final version

---
+++ Assets/Scripts/CameraController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+        
+private var offset : Vector3;
+        var player : GameObject;
+
+function Start () {
+  offset = transform.position;
+
+}
+
+function LateUpdate () {
+  transform.position = player.transform.position + offset;
+}
---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+var speed : float;
+var countText : GUIText;
+var winText : GUIText;
+
+private var count : int;
+
+
+function restartGame() {
+  speed = 0;
+  count = 0;
+  countText.text = "Count: 0";
+  winText.text = "0";
+  Application.LoadLevel(0);
+}
+
+function updateCountText() {
+  countText.text = "Count:" + count.ToString();
+  if(count >= 8) {
+    winText.text = "You win!";
+    
+    yield WaitForSeconds (5);
+    restartGame();
+  }
+}
+
+function Start () {
+  count = 0;
+  updateCountText(); 
+}
+
+function Update () {
+
+}
+
+function FixedUpdate () {
+  var moveHorizontal = Input.GetAxis("Horizontal");
+  var moveVertical =  Input.GetAxis("Vertical");
+  var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+  rigidbody.AddForce (movement * speed * Time.deltaTime);
+}
+
+function OnTriggerEnter (other : Collider) {
+  if(other.gameObject.tag == 'PickUp'){
+    count += 1;
+    other.gameObject.SetActive(false);
+
+    updateCountText();
+  }
+
+
+
+}
